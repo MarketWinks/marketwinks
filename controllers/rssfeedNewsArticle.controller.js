7033b4788ae834d8f0af8e1f6268b1fcd700c39b
Chocks <chocku.engr@gmail.com>
2020-07-18 13:31:23 +0100

    refactoring

--- controllers/rssfeedNewsArticle.controller.js
+++ controllers/rssfeedNewsArticle.controller.js
@@ -9,13 +9,13 @@ module.exports.rssfeedNewsArticleProfile = (req, res, next) =>{
     console.log("trying to reach rssfeed");
   
     //RssfeedNewsArticle.find({}).sort({"_id": -1}).limit(100, (err, rssfeedNewsArticle) => {
-        RssfeedNewsArticle.find({},null,{sort: {time: -1}}, (err, rssfeedNewsArticle) => {
+        RssfeedNewsArticle.find({},null,{sort: {time: -1}, limit: 20}, (err, rssfeedNewsArticle) => {
         if (err){
             console.log(err);
             res.send(err);
         
         } else {
-        rssfeedNewsArticle = rssfeedNewsArticle.slice(0,30);
+        //rssfeedNewsArticle = rssfeedNewsArticle.slice(0,20);
         console.log("Here we get");
         res.send(rssfeedNewsArticle);
     }
